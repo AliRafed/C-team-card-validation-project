97c2be41c9aab5256f241e555666deeb9209e481
AliRafed <162370489+AliRafed@users.noreply.github.com>
2024-03-05 22:49:25 +0300

    Update prefixMatched.py

--- prefixMatched.py
+++ prefixMatched.py
@@ -2,7 +2,7 @@
 def prefixMatched(number):
     firstNumber = number[0]
     first_2_Numbers = number[:2]
-    if firstNumber == "4" or firstNumber == '5' or firstNumber == '6' or first_2_Numbers == '37':
+    if firstNumber == '4' or firstNumber == '5' or firstNumber == '6' or first_2_Numbers == '37':
       return True
     else:
         return False
